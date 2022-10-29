89404e2983025ebfe3d69ce5782901bd02a48a6b
Sargis <s.mkoyan18@gmail.com>
2022-10-29 21:03:01 +0400

    cleanup

--- api/db/client.js
+++ api/db/client.js
@@ -1,18 +1,18 @@
 const { Sequelize } = require('sequelize');
+const dbConfig = require('../config').db;
 
 
-const database = 'renderforest_online_shop';
-const username = 'root';
-const password = '';
+const host = dbConfig.host;
+const database = dbConfig.database;
+const username = dbConfig.username;
+const password = dbConfig.password;
 
-const host = '127.0.0.1'; // fails to connect to localhost
 const dialect = 'mysql';
 
 const sequelize = new Sequelize(database, username, password, {
     host,
     dialect,
 });
-
 
 sequelize.authenticate().then(() => {
     console.log('Connection has been established successfully.');
