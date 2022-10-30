706d4fe636e8ea2de8a1ed24aac625bda0f45791
Sargis <s.mkoyan18@gmail.com>
2022-10-30 15:25:03 +0400

    [managers:category] implement method to fetch hierarchy of categories

--- api/managers/category.manager.js
+++ api/managers/category.manager.js
@@ -1,4 +1,6 @@
 const Category = require('../models/category.model');
+const sequelize = require('../db/client');
+const { QueryTypes } = require('sequelize');
 
 exports.storeCategory = async name => {
     const category = await Category.create({name});
@@ -21,4 +23,28 @@ exports.storeSubcategory = async (name, parentId) => {
 
     const category = await Category.create({name, parentId});
     return category.id;
+};
+
+exports.getParentCategories = async id => {
+    const query = `
+                with recursive parent_categories(id, name, parentId) as (
+                    select id, name, parentId
+                    from Categories
+                    where id = ?
+                    union all
+                    select Categories.id, Categories.name, Categories.parentId
+                    from Categories
+                    join parent_categories on parent_categories.parentId = Categories.id
+                )
+                select id, name, parentId
+                from parent_categories
+            `;
+
+    const categories = await sequelize.query(query, {
+        type: QueryTypes.SELECT,
+        model: Category,
+        replacements: [id],
+    });
+
+    return categories.length ? categories : null;
 }
